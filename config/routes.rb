9fea92c836e1d12f2c2131e7638fe4fdb17eb4ed
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-07-24 18:50:54 +0200

    added route to projects

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/home'
   get 'pages/about'
+  get 'pages/projects'
 
   resources :blogs, only: [:create, :new, :show, :destroy, :index, :update]
 #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
