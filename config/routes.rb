279d02e34baaffaea76c8f2624229ea7d06d028f
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-07-24 21:24:37 +0200

    added route to web development page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/projects'
+  get 'pages/web_development'
 
   resources :blogs, only: [:create, :new, :show, :destroy, :index, :update]
 #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
